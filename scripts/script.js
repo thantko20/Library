6fa09cc1cba406a895167a22f2a9baf71d4284a4
Thant Ko Zaw <thantmkou10@gmail.com>
2022-04-25 17:56:53 +0630

    Create Book Object

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
 let library = [];
 const addBookBtn = document.querySelector('.add-book-btn');
 const bookShelf = document.querySelector('.books-container');
+
+// BOOK OBJECT
+const Book = function([title, author, pages, readState]) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readState = readState;
+}
+
+Book.prototype.changeReadState = function() {
+  this.readState = !this.readState;
+}
